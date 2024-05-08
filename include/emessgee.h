a16f157b12f77385c416dfeff0f9e11008f7ceb7
will <willjgale@outlook.com>
2024-05-08 23:08:46 +0100

    Update includes

--- include/emessgee.h
+++ include/emessgee.h
@@ -1,2 +1,9 @@
 #include <publisher.h>
 #include <subscriber.h>
+#include <typedefs.h>
+#include <read_result.h>
+#include <write_memory_block.h>
+#include <read_memory_block.h>
+#include <write_memory_queue.h>
+#include <read_memory_queue.h>
+#include <buffer_write_code.h>
