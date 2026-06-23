c55e502f6597ef82dbef14bcca36e17f930adba0
will <will-j-gale@outlook.com>
2026-06-23 16:01:47 +0100

    Fix python param example

--- examples/params_get_set.py
+++ examples/params_get_set.py
@@ -1,20 +1,42 @@
-import time
 import os
 import sys
+import time
+from threading import Thread
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from emessgee import Params
+PARAM_NAME = "test_param"
+TERMINATE = "terminate"
 
+def write_thread(num_loops=10):
+    params = Params()
 
-#Act
-
+    for i in range(num_loops):
+        params.write_int(PARAM_NAME, i)
+        print(f"Wrote param: {i}")
+        time.sleep(1)
+    
+    params.write_bool(TERMINATE, True)
+    print("Write finished")
 
 def main():
-    key = "key_1"
     params = Params()
-    print("Reading!")
-    read_value = params.read_int(key)
+    test = False
+    params.write_bool(TERMINATE, test)
+    params.write_int(PARAM_NAME, 0)
 
-    print(read_value)
+    t = Thread(target=write_thread, args=(5, ))
+    t.start()
+
+    term = False
+    while(not term):
+        term = params.read_bool(TERMINATE)
+        int_param = params.read_int(PARAM_NAME)
+
+        print(f"Read: {int_param}")
+        time.sleep(0.2)
+    
+    t.join()
 
 if __name__ == "__main__":
     main()
